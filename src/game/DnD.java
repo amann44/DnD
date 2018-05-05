aa9997b1374dd62fea9c85df3844e8cdd56fb5ae
amann44 <amanning44@gmail.com>
2018-05-05 14:23:00 -0400

    Cleaning comments
    Cleaned up old comments used for feature testing

--- src/game/DnD.java
+++ src/game/DnD.java
@@ -6,42 +6,9 @@ import game.CharacterInformation;
 
 public class DnD {
 	public static void main(String[] args) {
-		/*String userInput; //User input
-		Scanner sc = new Scanner(System.in);
-		CharacterInformation pc = new CharacterInformation();
-		System.out.println(pc.strMod);
-		pc.setModifiers();
-		System.out.println(pc.strMod);
-		pc.strength = 24;
-		System.out.println(pc.strMod);
-		pc.setModifiers();
-		System.out.println(pc.strMod);
-		*/
-		
+
 		Character ch = new Character();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/// String input -  String name = sc.nextLine();
-		
-		
-		/*
-		System.out.println("Welcome to DnD!" + "\n" + ">>new - create new character" + "\n"+
-		">>load - load existing character");
-		*/
-		
-		/*
-		System.out.println("Welcome to DnD!" + "\n" + ">>continue" + "\n" + ">>exit");
-		userInput = sc.nextLine();
-		System.out.println(userInput);
-		*/
+
 		
 	}
 }
